be259b38f42261f2697239a2bf909c89a12f6b2c
Briter Gonzalez <briter.gd@gmail.com>
2025-09-19 12:54:06 +0200

    First commit

---
+++ src/components/Map/MapView.tsx
@@ -0,0 +1,38 @@
+// src/MapComponent.tsx
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+
+// Fix default marker icon issue
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+});
+
+const MapView: React.FC = () => {
+  const position: [number, number] = [37.7749, -122.4194]; // San Francisco
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "100vh", width: "100vw" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        />
+      <Marker position={position}>
+        <Popup>Hello! This is San Francisco.</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapView;
