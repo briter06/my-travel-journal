29a1d2d01a760a9591282ce13d611d31324af03e
Briter Gonzalez <briter.gd@gmail.com>
2025-10-02 13:45:49 +0200

    Change host url

--- packages/client/src/env/environment.ts
+++ packages/client/src/env/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
+  // apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://my-travel-journal-1e75012f20b3.herokuapp.com/api',
 };
